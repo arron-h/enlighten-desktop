4cfe687b77d1030a4403f37dd47e0dc0b2879710
Arron Hartley <arronhartley@gmail.com>
2015-01-08 22:45:29 +0000

    Changed lrprev_test to use parameterized fixtures.

--- test/spec/lrprev_test.cpp
+++ test/spec/lrprev_test.cpp
@@ -10,54 +10,58 @@ static const char* LrPrev_ValidPreviewFiles[] =
 	"catalogs/Lightroom 5 Catalog Previews.lrdata/B/B089/B089021B-7ACE-4A62-BD32-85A6C6AD5B9C-07cc63f155500a902b21fef7be6585b5.lrprev"
 };
 
-TEST(LrPrev, ShouldFailToInitialiseWithAnInvalidFile)
+class LrPrevTest : public ::testing::TestWithParam<const char*>
+{
+};
+
+TEST_F(LrPrevTest, ShouldFailToInitialiseWithAnInvalidFile)
 {
 	LrPrev lrPrev;
 	EXPECT_FALSE(lrPrev.initialiseWithFile("somefile.lrprev"));
 	EXPECT_FALSE(lrPrev.initialiseWithFile(NULL));
+}
+
+TEST_F(LrPrevTest, ShouldFailToInitialiseWhenFileIsNotAnLrPrevFile)
+{
+	LrPrev lrPrev;
 	EXPECT_FALSE(lrPrev.initialiseWithFile("emptyfile.lrprev"));
 }
 
-TEST(LrPrev, ShouldInitialiseWithAValidFile)
+TEST_F(LrPrevTest, ShouldInitialiseWithAValidFile)
 {
 	LrPrev lrPrev;
 	EXPECT_TRUE(lrPrev.initialiseWithFile(LrPrev_ValidPreviewFiles[0]));
 }
 
-TEST(LrPrev, ShouldExtractJpegFromAValidLevel)
+TEST_P(LrPrevTest, ShouldExtractJpegFromAValidLevel)
 {
-	int numberOfTestFiles = sizeof(LrPrev_ValidPreviewFiles) / sizeof(LrPrev_ValidPreviewFiles[0]);
+	LrPrev lrPrev;
+	lrPrev.initialiseWithFile(GetParam());
 
-	for (int i = 0; i < numberOfTestFiles; ++i)
-	{
-		LrPrev lrPrev;
-		lrPrev.initialiseWithFile(LrPrev_ValidPreviewFiles[i]);
+	unsigned int numberOfJpegBytes = 0;
+	unsigned char* bytes = lrPrev.extractFromLevel(1, numberOfJpegBytes);
 
-		unsigned int numberOfJpegBytes = 0;
-		unsigned char* bytes = lrPrev.extractFromLevel(1, numberOfJpegBytes);
+	ASSERT_TRUE(bytes != NULL);
+	ASSERT_GT(numberOfJpegBytes, 2);
 
-		ASSERT_TRUE(bytes != NULL);
-		ASSERT_GT(numberOfJpegBytes, 2);
+	/*
+	 Do a quick sanity check that the Jpeg marker bytes
+	 are correct. We should probably check against a GM
+	 image or something.
+	 */
 
-		/*
-		 Do a quick sanity check that the Jpeg marker bytes
-		 are correct. We should probably check against a GM
-		 image or something.
-		 */
+	// Start of Image
+	EXPECT_EQ(0xFF, bytes[0]);
+	EXPECT_EQ(0xD8, bytes[1]);
 
-		// Start of Image
-		EXPECT_EQ(0xFF, bytes[0]);
-		EXPECT_EQ(0xD8, bytes[1]);
+	// End of Image
+	EXPECT_EQ(0xFF, bytes[numberOfJpegBytes-2]);
+	EXPECT_EQ(0xD9, bytes[numberOfJpegBytes-1]);
 
-		// End of Image
-		EXPECT_EQ(0xFF, bytes[numberOfJpegBytes-2]);
-		EXPECT_EQ(0xD9, bytes[numberOfJpegBytes-1]);
-
-		free(bytes);
-	}
+	free(bytes);
 }
 
-TEST(LrPrev, ShouldReturnNullIfLevelDoesNotExist)
+TEST_F(LrPrevTest, ShouldReturnNullIfLevelDoesNotExist)
 {
 	LrPrev lrPrev;
 	lrPrev.initialiseWithFile(LrPrev_ValidPreviewFiles[0]);
@@ -66,3 +70,5 @@ TEST(LrPrev, ShouldReturnNullIfLevelDoesNotExist)
 	EXPECT_TRUE(lrPrev.extractFromLevel(9, numberOfJpegBytes) == NULL);
 	EXPECT_EQ(0, numberOfJpegBytes);
 }
+
+INSTANTIATE_TEST_CASE_P( , LrPrevTest, ::testing::ValuesIn(LrPrev_ValidPreviewFiles));
